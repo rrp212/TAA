aff5528236cd78ddddd9520fbcb6b58e7084dc41
boilnkettle <jmiguelsramos@gmail.com>
2020-03-19 21:43:42 +0000

    Change code layout

--- SplayNode.h
+++ SplayNode.h
@@ -10,7 +10,6 @@ public:
   using BSNode<T>::parent;
   using BSNode<T>::left;
   using BSNode<T>::right;
-
   using BSNode<T>::update_size;
   
   SplayNode(T v): BSNode<T>(v)
@@ -21,37 +20,15 @@ public:
   virtual SplayNode* insert(const T& v) override;
   virtual SplayNode* remove(const T& v) override;
 
+  virtual SplayNode*  rotate_left() override; 
+  virtual SplayNode* rotate_right() override;
+
   // Lookup
   virtual bool count(const T& v) override;
 
 private:
-  virtual SplayNode*  rotate_left() override; 
-  virtual SplayNode* rotate_right() override;
-  
   void splay();
 };
-
-
-template <class T>
-SplayNode<T>* SplayNode<T>::rotate_left()
-{
-  return (SplayNode<T>*) BSNode<T>::rotate_left();
-}
-
-
-template <class T>
-SplayNode<T>* SplayNode<T>::rotate_right()
-{
-  return (SplayNode<T>*) BSNode<T>::rotate_right();
-}
-
-
-template <class T>
-SplayNode<T>* SplayNode<T>::remove(const T& v)
-{
-  SplayNode<T>* t = SplayNode<T>::insert(v);
-  return (SplayNode*) t->BSNode<T>::remove(v);
-}
 
 
 template <class T>
@@ -79,6 +56,28 @@ SplayNode<T>* SplayNode<T>::insert(const T& v)
   update_size();
 
   return t;
+}
+
+
+template <class T>
+SplayNode<T>* SplayNode<T>::remove(const T& v)
+{
+  SplayNode<T>* t = SplayNode<T>::insert(v);
+  return (SplayNode*) t->BSNode<T>::remove(v);
+}
+
+
+template <class T>
+SplayNode<T>* SplayNode<T>::rotate_left()
+{
+  return (SplayNode<T>*) BSNode<T>::rotate_left();
+}
+
+
+template <class T>
+SplayNode<T>* SplayNode<T>::rotate_right()
+{
+  return (SplayNode<T>*) BSNode<T>::rotate_right();
 }
 
 
