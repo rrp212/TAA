274b60e4cbced40d89830d6f1bc9c430564e0cf4
boilnkettle <jmiguelsramos@gmail.com>
2020-04-04 21:26:30 +0100

    Restructure directories

--- test.cpp
+++ test.cpp
@@ -1,93 +1,138 @@
 #include <iostream>
 #include <string>
 
-#include "Tree.h"
-#include "BSNode.h"
+#include "lib/Tree.h"
+#include "lib/BSNode.h"
+#include "lib/AVLNode.h"
+#include "lib/SplayNode.h"
+#include "lib/TreapNode.h"
+#include "lib/SkipList.h"
+#include "lib/STL.h"
+
 
 int main()
 {
-  Tree<int, BSNode<int> > t;
-  std::string command;
+  srand(42);
+  std::string impl = "None";
+  // Uncomment for interactive testing
+  //
+  //  while(impl!="Tree" && impl!="SkipList" && impl!="STL")
+  //    {
+  //      printf("Choose set implementation (Tree, SkipList, STL)\n");
+      std::cin >> impl;
+  //    }
+
+  std::string node = "None";
+  if(impl=="Tree")
+    // Uncomment for interactive testing
+    //
+    //    while(node!="BS" && node!="AVL" && node!="Splay" && node!="Treap")
+    //      {
+    //	printf("Choose node implementation (BS, AVL, Splay, Treap)\n");
+	std::cin >> node;
+    //      }
   
+  Set<int> *S;
+  Tree<int, BSNode<int> >    tbs;
+  Tree<int, AVLNode<int> >   tavl;
+  Tree<int, SplayNode<int> > tspl;
+  Tree<int, TreapNode<int> > ttrp;
+  SkipList<int> skip;
+  STL<int> base;
+
+  if(impl=="Tree" && node=="BS")
+    S = &tbs;
+  else if(impl=="Tree" && node=="AVL")
+    S = &tavl;
+  else if(impl=="Tree" && node=="Splay")
+    S = &tspl;
+  else if(impl=="Tree" && node=="Treap")
+    S = &ttrp;
+  else if(impl=="SkipList")
+    S = &skip;
+  else
+    S = &base;
+  
+  std::string command;
   while(1)
     {
       std::cin >> command;
       if(command == "empty")
 	{
-	  printf("t is%s empty\n", t.empty()? " ":" not");
+	  printf("%s\n", S->empty()? "true":"false");
 	}
       else if(command == "size")
 	{
-	  printf("size: %d\n", t.size());
+	  printf("%d\n", S->size());
 	}
       else if(command == "clear")
 	{
-	  t.clear().print();
+	  S->clear();
+	  printf("clear\n");
 	}
       else if(command == "insert")
 	{
-	  int a; std::cin >> a;
-	  t.insert(a).print();
+	  int v; std::cin >> v;
+	  S->insert(v);
+	  printf("insert %d\n", v);
 	}
       else if(command == "remove")
 	{
-	  int a; std::cin >> a;
-	  t.remove(a).print();
+	  int v; std::cin >> v;
+	  S->remove(v);
+	  printf("remove %d\n", v);
 	}
       else if(command == "count")
 	{
-	  int a; std::cin >> a;
-	  printf("%d%s found\n", a, t.count(a)? "":" not");
+	  int v; std::cin >> v;
+	  printf("%s\n", S->count(v)? "true":"false");
 	}
       else if(command == "lower_bound")
 	{
-	  printf("not done yet\n");
+	  int v; std::cin >> v;
+	  auto ans = S->lower_bound(v);
+	  if(ans.second)
+	    printf("%d\n", ans.first);
+	  else
+	    printf("false\n");
 	}
       else if(command == "upper_bound")
 	{
-	  printf("not done yet\n");
+	  int v; std::cin >> v;
+	  auto ans = S->upper_bound(v);
+	  if(ans.second)
+	    printf("%d\n", ans.first);
+	  else
+	    printf("false\n");
 	}
       else if(command == "range_search")
 	{
-	  printf("not done yet\n");
+	  int l,r; std::cin >> l >> r;
+	  printf("%s\n", S->range_search(l,r)? "true":"false");
 	}
       else if(command == "min")
 	{
-	  if(t.min().second)
-	    printf("min: %d\n", t.min().first);
+	  auto ans = S->min();
+	  if(ans.second)
+	    printf("%d\n", ans.first);
 	  else
-	    printf("empty\n");
+	    printf("false\n");
 	}
       else if(command == "max")
 	{
-	  if(t.max().second)
-	    printf("max: %d\n", t.max().first);
+	  auto ans = S->max();
+	  if(ans.second)
+	    printf("%d\n", ans.first);
 	  else
-	    printf("empty\n");
+	    printf("false\n");
 	}
       else if(command == "print")
 	{
 	  int a; std::cin >> a;
-	  t.print(a);
+	  S->print(a);
 	}
-      else if(command == "rotate_left")
-	{
-	  int a; std::cin >> a;
-	  if(t.root->value == a)
-	    t.root = t.root->rotate_left();
-	  else
-	    t.root->insert(a)->rotate_left();
-	  t.print();
-	}
-      else if(command == "rotate_right")
-	{
-	  int a; std::cin >> a;
-	  if(t.root->value == a)
-	    t.root = t.root->rotate_right();
-	  else
-	    t.root->insert(a)->rotate_right();
-	  t.print();
-	}
+      else if(command == "quit")
+	break;
       else
 	printf("command not defined\n");
     }
