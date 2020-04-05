9099059a937e3a4bc9afd550b92170048604ae37
boilnkettle <jmiguelsramos@gmail.com>
2020-04-05 10:52:09 +0100

    Improve run_tests.sh output

--- run_tests.sh
+++ run_tests.sh
@@ -43,25 +43,33 @@ echo "Compiling test.cpp...";
 g++ test.cpp -o test;
 
 echo "Generating test files...";
-mkdir ./tmp;
-echo "STL" | cat > ./tmp/test_STL.in && cat "$test_file" >> ./tmp/test_STL.in;
-echo -e "Tree\nBS" | cat > ./tmp/test_BS.in && cat "$test_file" >> ./tmp/test_BS.in;
-echo -e "Tree\nAVL" | cat > ./tmp/test_AVL.in && cat "$test_file" >> ./tmp/test_AVL.in;
-echo -e "Tree\nSplay" | cat > ./tmp/test_Splay.in && cat "$test_file" >> ./tmp/test_Splay.in;
-echo -e "Tree\nTreap" | cat > ./tmp/test_Treap.in && cat "$test_file" >> ./tmp/test_Treap.in;
-echo "SkipList" | cat > ./tmp/test_SkipList.in && cat "$test_file" >> ./tmp/test_SkipList.in;
+TMP_DIR="./.tmp";
+mkdir "$TMP_DIR";
+echo "STL" | cat > "$TMP_DIR/test_STL.in" && cat "$test_file" >> "$TMP_DIR/test_STL.in";
+echo -e "Tree\nBS" | cat > "$TMP_DIR/test_BS.in" && cat "$test_file" >> "$TMP_DIR/test_BS.in";
+echo -e "Tree\nAVL" | cat > "$TMP_DIR/test_AVL.in" && cat "$test_file" >> "$TMP_DIR/test_AVL.in";
+echo -e "Tree\nSplay" | cat > "$TMP_DIR/test_Splay.in" && cat "$test_file" >> "$TMP_DIR/test_Splay.in";
+echo -e "Tree\nTreap" | cat > "$TMP_DIR/test_Treap.in" && cat "$test_file" >> "$TMP_DIR/test_Treap.in";
+echo "SkipList" | cat > "$TMP_DIR/test_SkipList.in" && cat "$test_file" >> "$TMP_DIR/test_SkipList.in";
 
-for file in ./tmp/*.in; do
-    echo "Running test for ${file%.*}...";
+for file in "$TMP_DIR"/*.in; do
+    echo "Running test for $(basename ${file%.*})...";
     ./test < "$file" > "${file%.*}.out";
 done;
 
-for file in ./tmp/*.out; do
-    echo "Comparing files $output_file and $file...";
-    diff "$output_file" "$file";
+RES_DIR="./results";
+mkdir -p "$RES_DIR";
+for file in "$TMP_DIR"/*.out; do
+    echo "Checking expected output for $(basename ${file%.*})...";
+    DIFF=$(diff "$output_file" "$file");
+    if [ "$DIFF" == "" ]; then
+	echo "All OK" | tee -a "$RES_DIR/$(basename ${file%.*})_$(date +"%Y%m%d%H%M%S").result";
+    else
+	echo "$DIFF" | tee -a "$RES_DIR/$(basename ${file%.*})_$(date +"%Y%m%d%H%M%S").result";
+    fi;
 done;
 
 echo "Cleaning up...";
-rm -rf ./tmp test;
+rm -rf "$TMP_DIR" test;
 
 exit 0;
